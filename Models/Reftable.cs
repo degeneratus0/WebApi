f13e533b73600217f27c391200e14905002fe765
Иван Егоров <iwannadie004@gmail.com>
2020-06-03 23:52:49 +0700

    EF model

---
+++ Models/Reftable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApi
+{
+    public partial class Reftable
+    {
+        public Reftable()
+        {
+            Maintable = new HashSet<Maintable>();
+        }
+
+        public int IdrefTable { get; set; }
+        public string Reference { get; set; }
+
+        public virtual ICollection<Maintable> Maintable { get; set; }
+    }
+}
